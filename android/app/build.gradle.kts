65bffdbb3792a62739e6bfdb3cdfc4e1f1235e1b
Foxtheory222 <foxtheory222@users.noreply.github.com>
2026-04-20 12:51:17 -0600

    Initial CTS Fluid Power Inspection App

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.ctsfluidpower.cts_fluid_power_inspection_app"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.ctsfluidpower.cts_fluid_power_inspection_app"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
---
+++ android/app/src/main/kotlin/com/ctsfluidpower/cts_fluid_power_inspection_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ctsfluidpower.cts_fluid_power_inspection_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
